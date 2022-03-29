021ce4f55a3b18f856e021dceba0763cd617a330
ArsenyAtman <arsenmail2018@gmail.com>
2022-03-30 00:21:32 +0300

    The AQuestActorsReferencer class is base for the AObjectiveMarkersManager class.

--- Source/MyGameSystem/QuestSystem/ObjectiveMarkersManager.h
+++ Source/MyGameSystem/QuestSystem/ObjectiveMarkersManager.h
@@ -3,24 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "QuestActorsReferencer.h"
 #include "ObjectiveMarkersManager.generated.h"
 
-UCLASS()
-class MYGAMESYSTEM_API AObjectiveMarkersManager : public AActor
+UCLASS(BlueprintType, Blueprintable)
+class MYGAMESYSTEM_API AObjectiveMarkersManager : public AQuestActorsReferencer
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
 	AObjectiveMarkersManager();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 };

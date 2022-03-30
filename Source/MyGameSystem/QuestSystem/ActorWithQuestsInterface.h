c7db6a1aaf46f5c95ed6f38abf8e29c9a735817f
ArsenyAtman <74785269+ArsenyAtman@users.noreply.github.com>
2022-03-30 21:03:09 +0300

    Add IActorWithQuestInterface interface.

---
+++ Source/MyGameSystem/QuestSystem/ActorWithQuestsInterface.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ActorWithQuestsInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UActorWithQuestsInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MYGAMESYSTEM_API IActorWithQuestsInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ActorWithQuestsInterface")
+	class UQuestComponent* GetActorQuestComponent();
+	virtual class UQuestComponent* GetActorQuestComponent_Implementation() { return nullptr; }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "ActorWithQuestsInterface")
+	class UMarkersManagerComponent* GetActorMarkersManagerComponent();
+	virtual class UMarkersManagerComponent* GetActorMarkersManagerComponent_Implementation() { return nullptr; }
+};

6544f378413b21106ac6d7fd55ce7a7787e50bc5
ArsenyAtman <74785269+ArsenyAtman@users.noreply.github.com>
2022-04-06 19:59:43 +0300

    Create a DialogSelection_WithTimer class

---
+++ Source/MyGameSystem/DialogSystem/DialogSelectionDataAsset_WithTimer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogSelectionDataAsset.h"
+#include "DialogSelectionDataAsset_WithTimer.generated.h"
+
+UCLASS()
+class MYGAMESYSTEM_API UDialogSelectionDataAsset_WithTimer : public UDialogSelectionDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Time = -1.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    int DefaultOption = 0;
+
+};

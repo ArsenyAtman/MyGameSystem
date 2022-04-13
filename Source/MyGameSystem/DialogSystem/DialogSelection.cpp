31cfb68ac99b9f78e71950c94bbe3812748849f7
ArsenyAtman <74785269+ArsenyAtman@users.noreply.github.com>
2022-04-13 14:43:40 +0300

    Rework loops of the DialogSelection class

--- Source/MyGameSystem/DialogSystem/DialogSelection.cpp
+++ Source/MyGameSystem/DialogSystem/DialogSelection.cpp
@@ -33,7 +33,7 @@ void UDialogSelection::SelectNextCue_Implementation(int CueIndex)
 TArray<TSubclassOf<UDialogCue>> UDialogSelection::GetAvailableOptions() const
 {
 	TArray<TSubclassOf<UDialogCue>> AvailableOptions;
-	for (TSubclassOf<UDialogCue>& Cue : SelectionData->Options)
+	for (const TSubclassOf<UDialogCue>& Cue : SelectionData->Options)
 	{
 		UDialogCue* CueObject = Cast<UDialogCue>(Cue.GetDefaultObject());
 		if (IsValid(CueObject) && CueObject->CheckAvailabilityCondition(OwningDialog))

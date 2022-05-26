6f506c30b1d80e0b4c55ef573f668ab338397936
ArsenyAtman <arsenmail2018@gmail.com>
2022-05-26 21:56:37 +0300

    refactor: Refactor the Objective class
    - UAdvancedActor is the base class now.
    - Reworked delegates.
    - Reworked the replication process.

--- Source/MyGameSystem/QuestSystem/Objective.cpp
+++ Source/MyGameSystem/QuestSystem/Objective.cpp
@@ -9,78 +9,114 @@
 #include "MarkersManagerComponent.h"
 #include "ActorWithQuestsInterface.h"
 #include "QuestActorsReferencer.h"
+#include "Net/UnrealNetwork.h"
+
+void UObjective::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UObjective, Condition);
+	DOREPLIFETIME(UObjective, ReferencesForQuest);
+}
 
 void UObjective::Activate_Implementation(UStage* RelatedStage)
 {
-	OwningStage = RelatedStage;
-	Condition = ETaskCondition::InProcess;
-	ReferencesForQuest = FindReferencesForQuest();
-	MarkersManager = CreateMarkersManager();
-	OnObjectiveActivated();
+	if(GetNetRole() == ENetRole::ROLE_Authority)
+	{
+		SetCondition(ETaskCondition::InProcess);
+		ReferencesForQuest = FindReferencesForQuest();
+		MarkersManager = CreateMarkersManager();
+		OnObjectiveActivated();
+	}
 }
 
 void UObjective::Abort_Implementation()
 {
-	if (Condition == ETaskCondition::InProcess)
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		Condition = ETaskCondition::Aborted;
-		EndObjective();
+		if (GetCondition() == ETaskCondition::InProcess)
+		{
+			SetCondition(ETaskCondition::Aborted);
+			EndObjective();
+		}
+		OnObjectiveAborted();
 	}
-	OnObjectiveAborted();
 }
 
 void UObjective::Mark()
 {
-	if(IsValid(MarkersManager))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		MarkersManager->MarkActors(FilterActorsForMarking(ReferencesForQuest.ActorsToMark));
+		if(IsValid(MarkersManager))
+		{
+			MarkersManager->MarkActors(FilterActorsForMarking(ReferencesForQuest.ActorsToMark));
+		}
 	}
 }
 
 void UObjective::Unmark()
 {
-	if(IsValid(MarkersManager))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		MarkersManager->DeleteAllMarkers();
+		if(IsValid(MarkersManager))
+		{
+			MarkersManager->DeleteAllMarkers();
+		}
 	}
 }
 
-FObjectiveInfo UObjective::GetObjectiveInfo() const
+UStage* UObjective::GetOwningStage() const
 {
-	return FObjectiveInfo(ObjectiveData, Condition, Progress);
+	return Cast<UStage>(GetOuter());
 }
 
 void UObjective::Update_Implementation()
 {
-	Unmark();
-	Mark();
-	OnObjectiveUpdated();
-	Progress = RecalculateProgress();
-	OwningStage->Update();
+	if(GetNetRole() == ENetRole::ROLE_Authority)
+	{
+		Unmark();
+		Mark();
+		OnUpdatedNotify();
+	}
 }
 
 void UObjective::EndObjective_Implementation()
 {
-	if(IsValid(MarkersManager))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		MarkersManager->DestroyComponent();
+		if(IsValid(MarkersManager))
+		{
+			MarkersManager->DestroyComponent();
+		}
 	}
+}
+
+void UObjective::SetCondition(ETaskCondition NewCondition)
+{
+	Condition = NewCondition;
+	ConditionChangeBroadcast(Condition);
 }
 
 void UObjective::Complete_Implementation()
 {
-	Condition = ETaskCondition::Completed;
-	EndObjective();
-	OnObjectiveCompleted();
-	OwningStage->ObjectiveCompleted(this);
+	if(GetNetRole() == ENetRole::ROLE_Authority)
+	{
+		SetCondition(ETaskCondition::Completed);
+		EndObjective();
+		OnObjectiveCompleted();
+		GetOwningStage()->ObjectiveCompleted(this);
+	}
 }
 
 void UObjective::Fail_Implementation()
 {
-	Condition = ETaskCondition::Failed;
-	EndObjective();
-	OnObjectiveFailed();
-	OwningStage->ObjectiveFailed(this);
+	if(GetNetRole() == ENetRole::ROLE_Authority)
+	{
+		SetCondition(ETaskCondition::Failed);
+		EndObjective();
+		OnObjectiveFailed();
+		GetOwningStage()->ObjectiveFailed(this);
+	}
 }
 
 FReferencesForQuest UObjective::FindReferencesForQuest() const
@@ -102,11 +138,60 @@ FReferencesForQuest UObjective::FindReferencesForQuest() const
 
 UMarkersManagerComponent* UObjective::CreateMarkersManager() const
 {
-	AActor* QuestActor = OwningStage->GetOwningQuest()->GetOwningQuestComponent()->GetOwner();
+	AActor* QuestActor = GetOwningStage()->GetOwningQuest()->GetOwningQuestComponent()->GetOwner();
 	if (IsValid(QuestActor))
 	{
 		return Cast<UMarkersManagerComponent>(QuestActor->AddComponentByClass(MarkersManagerComponentClass, false, FTransform::Identity, false));
 	}
 
 	return nullptr;
+}
+
+void UObjective::OnUpdatedNotify_Implementation()
+{
+	if(OnUpdated.IsBound())
+	{
+		OnUpdated.Broadcast(this);
+	}
+}
+
+void UObjective::OnRep_Condition()
+{
+	ConditionChangeBroadcast(GetCondition());
+}
+
+void UObjective::ConditionChangeBroadcast(ETaskCondition CurrentCondition)
+{
+	switch(CurrentCondition)
+	{
+		case ETaskCondition::InProcess:
+
+			OnActivated.Broadcast(this);
+
+			break;
+
+		case ETaskCondition::Completed:
+
+			OnCompleted.Broadcast(this);
+
+			break;
+
+		case ETaskCondition::Failed:
+
+			OnFailed.Broadcast(this);
+
+			break;
+
+		case ETaskCondition::Aborted:
+
+			OnAborted.Broadcast(this);
+
+			break;
+
+		default:
+
+			// ...
+
+			break;
+	}
 }

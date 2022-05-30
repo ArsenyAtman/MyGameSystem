01526987ef036dc8efa6017645b0869b42aecadb
ArsenyAtman <arsenmail2018@gmail.com>
2022-05-30 17:56:08 +0300

    refactor: Remove CraftingSystem

--- Source/MyGameSystem/CraftingSystem/CraftActorInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CraftActorInterface.h"
-
-// Add default functionality here for any ICraftActorInterface functions that are not pure virtual.

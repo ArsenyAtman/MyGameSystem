700f201c8d38b8f8b4f594a4844c6a34a75b30ca
ArsenyAtman <arsenmail2018@gmail.com>
2022-05-31 20:09:41 +0300

    refactor: Rework Stat
    - Make it replicable
    - Rework delegates

--- Source/MyGameSystem/StatsSystem/Stat.cpp
+++ Source/MyGameSystem/StatsSystem/Stat.cpp
@@ -6,73 +6,59 @@
 #include "StatDeltaApplier.h"
 #include "StatsComponent.h"
 #include "Effect.h"
-#include "StatDataAsset.h"
-#include "Kismet/KismetSystemLibrary.h"
+#include "Net/UnrealNetwork.h"
+
+void UStat::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UStat, StatValues);
+	DOREPLIFETIME(UStat, Effects);
+}
 
 FStatValues UStat::ApplyDelta(FStatValues Delta, UEffect* Effect)
 {
-	if (IsValid(StatDeltaApplier))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		FStatValues ActualDelta = StatDeltaApplier->ApplyDelta(Delta, Effect);
-		StatValues = StatValues + ActualDelta;
-
-		FStatValues OldStatValues = StatValues;
-
-		StatValues.Value = FMath::Clamp(StatValues.Value, StatValues.Min, StatValues.Max);
-
-		ActualDelta = StatValues - OldStatValues;
-
-		if (OnValuesChanged.IsBound())
+		if (IsValid(StatDeltaApplier))
 		{
-			OnValuesChanged.Broadcast(ActualDelta, Effect);
-		}
+			FStatValues ModifiedDelta = StatDeltaApplier->ApplyDelta(Delta, Effect);
+			FStatValues OldStatValues = GetStatValues();
+			FStatValues NewStatValues = OldStatValues + ModifiedDelta;
 
-		if (StatValues.Min >= StatValues.Value)
-		{
-			if (OnMinReached.IsBound())
-			{
-				OnMinReached.Broadcast();
-			}
-		}
-		else if (StatValues.Max <= StatValues.Value)
-		{
-			if (OnMaxReached.IsBound())
-			{
-				OnMaxReached.Broadcast();
-			}
-		}
+			SetStatValues(NewStatValues);
 
-		return ActualDelta;
+			FStatValues ActualDelta = StatValues - OldStatValues;
+			return ActualDelta;
+		}
 	}
-	else
-	{
-		return FStatValues();
-	}
+
+	return FStatValues();
 }
 
 void UStat::AddEffect(UEffect* Effect)
 {
-	if (IsValid(Effect))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		Effects.Add(Effect);
-	}
+		if (IsValid(Effect))
+		{
+			Effects.Add(Effect);
 
-	if (OnEffectAdded.IsBound())
-	{
-		OnEffectAdded.Broadcast(Effect);
+			Broadcast_OnEffectAdded(Effect);
+		}
 	}
 }
 
 void UStat::RemoveEffect(UEffect* Effect)
 {
-	if (IsValid(Effect))
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		Effects.Remove(Effect);
-	}
+		if (IsValid(Effect))
+		{
+			Effects.Remove(Effect);
 
-	if (OnEffectRemoved.IsBound())
-	{
-		OnEffectRemoved.Broadcast(Effect);
+			Broadcast_OnEffectRemoved(Effect);
+		}
 	}
 }
 
@@ -81,14 +67,86 @@ UStatsComponent* UStat::GetOwningStatsComponent() const
 	return Cast<UStatsComponent>(GetOuter());
 }
 
-FStatInfo UStat::GetStatInfo() const
+void UStat::SetStatValues(FStatValues NewStatValues)
 {
-	TArray<FEffectInfo> EffectInfos;
-
-	for (const UEffect* Effect : GetEffects())
+	if(GetNetRole() == ENetRole::ROLE_Authority)
 	{
-		EffectInfos.Add(Effect->GetEffectInfo());
+		FStatValues PrevValues = StatValues;
+
+		StatValues = NewStatValues;
+		StatValues.Value = FMath::Clamp(StatValues.Value, StatValues.Min, StatValues.Max);
+
+		BroadcastChange_StatValues(PrevValues);
+	}
+}
+
+void UStat::OnRep_StatValues(FStatValues PreReplicationStatValues)
+{
+	BroadcastChange_StatValues(PreReplicationStatValues);
+}
+
+void UStat::BroadcastChange_StatValues(FStatValues PrevValues)
+{
+	FStatValues CurrentValues = GetStatValues();
+
+	if(PrevValues != CurrentValues)
+	{
+		OnValuesChanged.Broadcast(this, CurrentValues - PrevValues);
+
+		if(CurrentValues.Value == CurrentValues.Min && (PrevValues.Value != CurrentValues.Value || PrevValues.Min != CurrentValues.Min))
+		{
+			OnMinReached.Broadcast(this);
+		}
+
+		if(CurrentValues.Value == CurrentValues.Max && (PrevValues.Value != CurrentValues.Value || PrevValues.Max != CurrentValues.Max))
+		{
+			OnMaxReached.Broadcast(this);
+		}
+	}
+}
+
+void UStat::OnRep_Effects(const TArray<UEffect*>& PreReplicationEffects)
+{
+	BroadcastChange_Effects(PreReplicationEffects);
+}
+
+void UStat::BroadcastChange_Effects(const TArray<UEffect*>& PrevEffects)
+{
+	TArray<UEffect*> AddedEffects = FindMissingEffects(Effects, PrevEffects);
+	TArray<UEffect*> RemovedEffects = FindMissingEffects(PrevEffects, Effects);
+
+	for(UEffect* Effect : AddedEffects)
+	{
+		Broadcast_OnEffectAdded(Effect);
 	}
 
-	return FStatInfo(StatData, StatBaseValues, StatValues, EffectInfos);
+	for(UEffect* Effect : RemovedEffects)
+	{
+		Broadcast_OnEffectRemoved(Effect);
+	}
+}
+
+void UStat::Broadcast_OnEffectAdded(UEffect* Effect)
+{
+	OnEffectAdded.Broadcast(this, Effect);
+}
+
+void UStat::Broadcast_OnEffectRemoved(UEffect* Effect)
+{
+	OnEffectRemoved.Broadcast(this, Effect);
+}
+
+TArray<UEffect*> UStat::FindMissingEffects(const TArray<UEffect*>& FromArray, const TArray<UEffect*>& InArray) const
+{
+	TArray<UEffect*> MissingEffects;
+
+	for(UEffect* Effect : FromArray)
+	{
+		if(InArray.Find(Effect) == INDEX_NONE)
+		{
+			MissingEffects.Add(Effect);
+		}
+	}
+
+	return MissingEffects;
 }

31b6b8310b947fe81fa8a41ec4e5001eb975f8fd
ArsenyAtman <arsenmail2018@gmail.com>
2022-05-31 20:09:12 +0300

    refactor: Rework Effect
    - Make it replicable
    - Add delegates

--- Source/MyGameSystem/StatsSystem/Effect.h
+++ Source/MyGameSystem/StatsSystem/Effect.h
@@ -3,52 +3,62 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "MyGameSystem/AdvancedObject/AdvancedObject.h"
 #include "StatsSystemTypes.h"
 #include "Effect.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEffectConditionChangeDelegate, class UEffect*, Effect);
+
 UCLASS(BlueprintType, Blueprintable)
-class MYGAMESYSTEM_API UEffect : public UObject
+class MYGAMESYSTEM_API UEffect : public UAdvancedObject
 {
 	GENERATED_BODY()
 
 public:
 
-	UFUNCTION(BlueprintCallable, Category = "Effect|Internal")
-	void Activate(class UStatsComponent* StatsComponent);
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
 	UFUNCTION(BlueprintCallable, Category = "Effect|Internal")
+	void Activate();
+
+	UFUNCTION(BlueprintCallable, Category = "Effect|Control")
 	void Deactivate();
 
 	UFUNCTION(BlueprintPure, Category = "Effect|RelatedStatsComponent")
-	class UStatsComponent* GetRelatedStatsComponent() const { return RelatedStatsComponent; }
+	class UStatsComponent* GetRelatedStatsComponent() const;
 
 	UFUNCTION(BlueprintPure, Category = "Effect|RelatedActor")
 	class AActor* GetRelatedActor() const;
 
-	UFUNCTION(BlueprintPure, Category = "Effect|Condition")
-	FEffectInfo GetEffectInfo() const;
+	UPROPERTY(BlueprintAssignable)
+	FEffectConditionChangeDelegate OnEffectActivated;
 
-	UFUNCTION(BlueprintPure, Category = "Effect|Condition")
-	bool GetIsActive() const { return bIsActive; }
+	UPROPERTY(BlueprintAssignable)
+	FEffectConditionChangeDelegate OnEffectDeactivated;
 
 protected:
 
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Effect|Control")
+	virtual void EndPlay_Implementation() override;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Effect|Control", meta = (BlueprintProtected))
 	void OnActivated();
 	virtual void OnActivated_Implementation() { return; }
 
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Effect|Control")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Effect|Control", meta = (BlueprintProtected))
 	void OnDeactivating();
 	virtual void OnDeactivating_Implementation() { return; }
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Effect|Condition")
-	class UEffectDataAsset* EffectData = nullptr;
 
 private:
 
 	class UStatsComponent* RelatedStatsComponent;
 
-	bool bIsActive = false;
+	UFUNCTION(NetMulticast, Reliable)
+	void Notify_OnActivated();
+
+	UFUNCTION()
+	void Broadcast_OnActivated();
+
+	UFUNCTION()
+	void Broadcast_OnDeactivated();
 	
 };

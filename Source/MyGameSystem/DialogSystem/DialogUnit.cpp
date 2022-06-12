5b795d6ab8307a90e9646fbdb98ef4234d648229
ArsenyAtman <arsenmail2018@gmail.com>
2022-06-12 13:46:46 +0300

    refactor: Rework DialogUnit
    - Make it replicable
    - Rework ownership

--- Source/MyGameSystem/DialogSystem/DialogUnit.cpp
+++ Source/MyGameSystem/DialogSystem/DialogUnit.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "DialogUnit.h"
-#include "DialogUnitDataAsset.h"
+#include "Dialog.h"
 
-// ...
+UDialog* UDialogUnit::GetOwningDialog() const
+{
+	return Cast<UDialog>(GetOuter());
+}
--- Source/MyGameSystem/DialogSystem/DialogUnit.h
+++ Source/MyGameSystem/DialogSystem/DialogUnit.h
@@ -3,28 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "MyGameSystem/AdvancedObject/AdvancedObject.h"
 #include "DialogUnit.generated.h"
 
 UCLASS(BlueprintType, Blueprintable, Abstract)
-class MYGAMESYSTEM_API UDialogUnit : public UObject
+class MYGAMESYSTEM_API UDialogUnit : public UAdvancedObject
 {
 	GENERATED_BODY()
 
 public:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "DialogUnit|Internal")
-	void Activate(class UDialog* OwnDialog);
-	virtual void Activate_Implementation(class UDialog* OwnDialog) { return; }
-
-	UFUNCTION(BlueprintPure, BlueprintNativeEvent, Category = "DialogUnit|Data")
-	class UDialogUnitDataAsset* GetDialogUnitData() const;
-	virtual class UDialogUnitDataAsset* GetDialogUnitData_Implementation() const { return nullptr; }
+	void Activate();
+	virtual void Activate_Implementation() { return; }
 
 	UFUNCTION(BlueprintPure, BlueprintNativeEvent, Category = "DialogUnit|Availability")
 	bool CheckAvailabilityCondition(class UDialog* CheckingDialog) const;
 	virtual bool CheckAvailabilityCondition_Implementation(class UDialog* CheckingDialog) const { return true; }
 
+	UFUNCTION(BlueprintPure, Category = "DialogUnit|OwningDialog")
+	class UDialog* GetOwningDialog() const;
+
+protected:
+
+	// ...
+
 private:
+
+	// ...
 
 };

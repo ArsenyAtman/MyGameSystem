5d07a487b85a6729ade0efb60608750d959e2349
ArsenyAtman <arsenmail2018@gmail.com>
2022-06-18 21:12:37 +0300

    feat: Add condition delegates.

--- Source/MyGameSystem/ActionSystem/ActorAction.h
+++ Source/MyGameSystem/ActionSystem/ActorAction.h
@@ -7,6 +7,12 @@
 #include "MyGameSystem/AdvancedObject/AdvancedObject.h"
 #include "ActorAction.generated.h"
 
+
+/**
+ * Delegate for handling changes of an ActorAction.
+ */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FActorActionConditionDelegate, class UActorAction*, ActorAction);
+
 /**
  * Some action that is limited in time. May have some logic and line up in combinations.
  */
@@ -99,6 +105,18 @@ public:
 	UFUNCTION(BlueprintGetter, Category = "ActorAction|ComboWindow")
 	FORCEINLINE bool GetComboWindowStarted() const { return bComboWindowStarted; }
 
+	/**
+	 * Called after the action start.
+	 */
+	UPROPERTY(BlueprintAssignable)
+	FActorActionConditionDelegate OnActorActionStarted;
+
+	/**
+	 * Called after the action end
+	 */
+	UPROPERTY(BlueprintAssignable)
+	FActorActionConditionDelegate OnActorActionEnded;
+
 protected:
 
 	/**
@@ -150,6 +168,18 @@ protected:
 	virtual void OnComboWindowEnded_Implementation() { return; }
 
 private:
+
+	// Notify all machines about the action start.
+	UFUNCTION(NetMulticast, Reliable)
+	void Notify_ActionStart();
+
+	// Notify all machines about the action end.
+	UFUNCTION(NetMulticast, Reliable)
+	void Notify_ActionEnd();
+
+	// Broadcast delegates.
+	void Broadcast_ActionStart();
+	void Broadcast_ActionEnd();
 
 	/**
 	 * Whether the action event started.
